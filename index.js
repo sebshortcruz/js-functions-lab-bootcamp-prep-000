9f8e3e1d7c5d63fc343864faf2c44d663e8ef6f9
Sebastian Short-Cruz <sebastian.shortcruz@gmail.com>
2020-05-21 13:12:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code below!
+function happyHolidays (string) {
+  return "Happy holidays!"  
+}
+
+function happyHolidaysTo(string) {
+  var name = "Nadira"
+  return (`Happy holidays, ${name}!`)
+}

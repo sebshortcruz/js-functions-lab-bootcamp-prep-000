a4396c0818eb417bf02f6f24c355788f2659a52d
Sebastian Short-Cruz <sebastian.shortcruz@gmail.com>
2020-05-21 13:38:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function happyCustomHolidayTo(holiday, name) {
   
   return (`Happy ${holiday}, ${name}!`) 
 }
+
+function holidayCountdown(holiday, days) {
+  return (`It's ${days} days until ${holiday}!`)
+}
